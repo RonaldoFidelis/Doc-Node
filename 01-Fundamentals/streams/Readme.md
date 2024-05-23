4a6f57373204ab1961d7e9ec5161c7a420b0acb3
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2024-05-23 17:57:59 -0300

    Commit: Create Streams

---
+++ 01-Fundamentals/streams/Readme.md
@@ -0,0 +1,3 @@
+# What is Streams in Node.Js?
+
+Em Node.js, streams são uma forma eficiente de lidar com dados de entrada e saída, especialmente quando se trata de operações que envolvem grandes volumes de dados. Uma stream é basicamente uma sequência de dados que é processada por pedaços, o que permite o processamento parcial dos dados antes de todo o conjunto ser carregado na memória.
--- 01-Fundamentals/streams/fundamentals.js
+++ 01-Fundamentals/streams/fundamentals.js
@@ -1,22 +1,39 @@
-import {Readable} from "node:stream"
+import { Readable, Writable, Transform } from 'node:stream';
 
-class oneToHundredStream extends Readable {
+class OneToTenStream extends Readable {
   index = 1;
 
   _read() {
     const i = this.index++;
 
     setTimeout(() => {
-      if(i > 100) {
-        this.push(null);
+      if (i > 10) {
+        this.push(null); // Indica o fim da stream
       } else {
-        const buffer = Buffer.from(String(i).concat(' '));
-        this.push(buffer)
+        const buffer = Buffer.from(String(i));
+        this.push(buffer);
       }
-    }, 1000);
+    }, 1000); // Atraso de 1 segundo entre os números
   }
-
 }
 
-new oneToHundredStream()
-.pipe(process.stdout);
+class TransformDataStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformedChunk = chunk.toString().concat(' oi').toUpperCase();
+		const buffer = Buffer.from(transformedChunk);
+    this.push(buffer);
+    callback();
+  }
+}
+
+class WritingStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(chunk.toString());
+    callback();
+  }
+}
+
+// Encadeando as streams: legível -> transform -> gravável
+new OneToTenStream()
+  .pipe(new TransformDataStream())
+  .pipe(new WritingStream());
